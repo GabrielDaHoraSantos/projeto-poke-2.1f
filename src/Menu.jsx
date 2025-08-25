42d7553f515a15869d2cd8c3f84ac86a99c8fcee
GabrielDaHoraSantos <rm95298@estudante.fieb.edu.br>
2025-08-25 12:15:35 +0000

    incompleto 25/08

---
+++ src/Menu.jsx
@@ -0,0 +1,19 @@
+const Menu = () => {
+
+    return (
+        <div>
+            <NavBar bg="light" expand="lg">
+                <ul>
+                    <li>
+                        <a href={"/"}>Pokemon</a>
+                    </li>
+                    <li>
+                        <a href={"#"}>ViaCep</a>
+                    </li>
+                </ul>
+            </NavBar>
+        </div>
+    )
+}
+
+export default Menu;
